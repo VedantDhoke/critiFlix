a301621154b3cfbcdb66be5d336b8e355d4c87f4
Shiven Bansal <shivenapj@gmail.com>
2025-04-05 19:30:09 +0530

    Movies section is connected to db .... users , reviews not

--- app.py
+++ app.py
@@ -1,32 +1,19 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.secret_key = 'testsecretkey'
 
-# Dummy Data
-users = []
-movies = [
-    {
-        'id': 1,
-        'title': 'Inception',
-        'genre': 'Sci-Fi',
-        'year': 2010,
-        'description': 'A mind-bending thriller',
-        'poster_url': 'https://image.tmdb.org/t/p/original/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg'
-    }
-]
-reviews = [
-    {
-        'movie_id': 1,
-        'username': 'admin',
-        'content': 'Amazing movie!'
-    }
-]
-
-# Routes
+client = MongoClient("mongodb+srv://shiven:shiven123@shivencluster.blo4awt.mongodb.net/?retryWrites=true&w=majority&appName=ShivenCluster")
+db = client["CritiFlix"]
+users_col = db["users"]
+movies_col = db["movies"]
+reviews_col = db["reviews"]
 
 @app.route('/')
 def index():
+    movies = list(movies_col.find())
     return render_template('index.html', movies=movies)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -34,17 +21,17 @@ def login():
     if request.method == 'POST':
         uname = request.form['username']
         pwd = request.form['password']
-        for user in users:
-            if user['username'] == uname and user['password'] == pwd:
-                session['username'] = uname
-                return redirect(url_for('index'))
+        user = users_col.find_one({'username': uname, 'password': pwd})
+        if user:
+            session['username'] = uname
+            return redirect(url_for('index'))
         flash('Invalid credentials')
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        users.append({
+        users_col.insert_one({
             'username': request.form['username'],
             'password': request.form['password'],
             'email': request.form['email']
@@ -64,9 +51,7 @@ def add_movie():
     if 'username' not in session:
         return redirect(url_for('login'))
     if request.method == 'POST':
-        new_id = len(movies) + 1
-        movies.append({
-            'id': new_id,
+        movies_col.insert_one({
             'title': request.form['title'],
             'genre': request.form['genre'],
             'year': request.form['year'],
@@ -76,58 +61,63 @@ def add_movie():
         return redirect(url_for('index'))
     return render_template('add_movie.html')
 
-@app.route('/edit_movie/<int:id>', methods=['GET', 'POST'])
+@app.route('/edit_movie/<id>', methods=['GET', 'POST'])
 def edit_movie(id):
-    movie = next((m for m in movies if m['id'] == id), None)
+    movie = movies_col.find_one({'_id': ObjectId(id)})
     if not movie:
         return "Movie not found", 404
     if request.method == 'POST':
-        movie['title'] = request.form['title']
-        movie['genre'] = request.form['genre']
-        movie['year'] = request.form['year']
-        movie['description'] = request.form['description']
-        movie['poster_url'] = request.form['poster_url']
+        movies_col.update_one({'_id': ObjectId(id)}, {
+            '$set': {
+                'title': request.form['title'],
+                'genre': request.form['genre'],
+                'year': request.form['year'],
+                'description': request.form['description'],
+                'poster_url': request.form['poster_url']
+            }
+        })
         return redirect(url_for('index'))
     return render_template('edit_movie.html', movie=movie)
 
-@app.route('/delete_movie/<int:id>')
+@app.route('/delete_movie/<id>')
 def delete_movie(id):
-    global movies
-    movies = [m for m in movies if m['id'] != id]
+    movies_col.delete_one({'_id': ObjectId(id)})
     return redirect(url_for('index'))
 
-@app.route('/movie/<int:id>')
+@app.route('/movie/<id>')
 def movie_detail(id):
-    movie = next((m for m in movies if m['id'] == id), None)
-    movie_reviews = [r for r in reviews if r['movie_id'] == id]
+    movie = movies_col.find_one({'_id': ObjectId(id)})
+    movie_reviews = list(reviews_col.find({'movie_id': ObjectId(id)}))
     return render_template('movie_detail.html', movie=movie, reviews=movie_reviews)
 
-@app.route('/add_review/<int:movie_id>', methods=['POST'])
-def add_review(movie_id):
+@app.route('/add_review/<id>', methods=['POST'])
+def add_review(id):
     if 'username' not in session:
         return redirect(url_for('login'))
     content = request.form['content']
-    reviews.append({
-        'movie_id': movie_id,
+    reviews_col.insert_one({
+        'movie_id': ObjectId(id),
         'username': session['username'],
         'content': content
     })
-    return redirect(url_for('movie_detail', id=movie_id))
+    return redirect(url_for('movie_detail', id=id))
 
-@app.route('/edit_review/<int:movie_id>/<int:review_index>', methods=['GET', 'POST'])
-def edit_review(movie_id, review_index):
-    review = [r for r in reviews if r['movie_id'] == movie_id][review_index]
+@app.route('/edit_review/<review_id>', methods=['GET', 'POST'])
+def edit_review(review_id):
+    review = reviews_col.find_one({'_id': ObjectId(review_id)})
+    if not review:
+        return "Review not found", 404
     if request.method == 'POST':
-        review['content'] = request.form['content']
-        return redirect(url_for('movie_detail', id=movie_id))
-    return render_template('edit_review.html', review=review, movie_id=movie_id, review_index=review_index)
+        reviews_col.update_one({'_id': ObjectId(review_id)}, {'$set': {'content': request.form['content']}})
+        return redirect(url_for('movie_detail', id=review['movie_id']))
+    return render_template('edit_review.html', review=review)
 
-@app.route('/delete_review/<int:movie_id>/<int:review_index>')
-def delete_review(movie_id, review_index):
-    review_list = [r for r in reviews if r['movie_id'] == movie_id]
-    if review_index < len(review_list):
-        reviews.remove(review_list[review_index])
-    return redirect(url_for('movie_detail', id=movie_id))
+@app.route('/delete_review/<review_id>')
+def delete_review(review_id):
+    review = reviews_col.find_one({'_id': ObjectId(review_id)})
+    if review:
+        reviews_col.delete_one({'_id': ObjectId(review_id)})
+    return redirect(url_for('movie_detail', id=review['movie_id']))
 
 if __name__ == '__main__':
     app.run(debug=True)
